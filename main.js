7478e17098de762a245a11b70428869b0cfc52bb
mithun-banerjee <mithunbanerjee632@gmail.com>
2022-03-25 01:13:27 +0600

    concating two arrays

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ function addnumber(x,y,z){
   return x+y+z;
 }
 
-let number=[1,2,3];
-console.log(addnumber(...number)); //passing value by sprade operator
+let number1=[1,2,3];
+//console.log(addnumber(...number)); //passing value by sprade operator
+let number2=[4,5,6];
+let number=[...number1,...number2]; //concating two arrays
+console.log(number);
+
 
