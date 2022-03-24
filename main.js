e20e3b5250a251d1cd2fb78b2a7bed79f63d61c5
mithun-banerjee <mithunbanerjee632@gmail.com>
2022-03-24 22:14:30 +0600

    rest parameter

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
-//without Sprade Operator: three dots(...)
-let poorCountry =['Bangladesh','Srilanka','Bhutan'];
-let richCountry =['America','Canada','Italy'];
-//console.log('richCountry')
-richCountry.push(poorCountry);
+//Rest pararmeter(...)
+
+function caluculation(...numbers){
+   let sum = 0;
+   for(let i of numbers){
+      sum =sum+i;
+   }
+   console.log(sum);
+}
+caluculation(1,2,3,4);
