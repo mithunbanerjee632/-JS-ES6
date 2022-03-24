21c70f712378cb715dea69255ef69061d50a8ee7
mithun-banerjee <mithunbanerjee632@gmail.com>
2022-03-25 02:35:06 +0600

    for and foreach loop in ES6

--- main.js
+++ main.js
@@ -1,21 +1,25 @@
-//for of loop
+//for vs foreach loop
 
-let names =['Mithun Banerjee','Puja Mukherjee','Modhuchandrima Banerjee'];
+//for loop
 
-for(let name of names){
-   console.log(name); //names er element gula ekta ekta kore name er vitor asbe
-}
+// var numbers =[10,20,30,40,50];
+// for(var x=0;x<numbers.length;x++){
+//    console.log(numbers[x]);
+// }
 
 
-//for in (object k iterate korar jonno use kora hoy)
 
-let details ={
-   id:141,
-   name:"mithun banerjee",
-   department:"cse"
-}
+//forEach loop
 
-for(let i in details){
-   //console.log(i);   //eta sudhu key return korbe
-   console.log(`${i}: ${details[i]}`);
-}
+var numbers =[10,20,30,40,50];
+
+// numbers.forEach(myfunction)     //foreach sobsomoy function receive kore
+
+// function myfunction(x){    //ekta ekta element myfunction e dhukbe r x parameter ta rcv kore print krb
+//    console.log(x);
+// }
+
+numbers.forEach(function(x){   //anonymous function
+   console.log(x);
+})
+
