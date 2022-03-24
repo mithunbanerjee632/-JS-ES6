4c908146597dcd82d63842478e4ff6c122eb3136
mithun-banerjee <mithunbanerjee632@gmail.com>
2022-03-25 00:43:25 +0600

    how to use function in ES6

--- main.js
+++ main.js
@@ -1,24 +1,8 @@
-//variables
+//function
 
-//let x=10;
-// if(true){
-//    let x=11;
-//    console.log(x); //ekhane sudhu ei variable ta kaj korbe
-// }
-//console.log(x); //eta if block er birer ta
-
-// const x=10;
-// if(true){
-//    x=11;   // const change korle value change kora jabe na
-// }
-// console.log(x);
-
-// let x= 20;
-// let y=10;
-// let sum=x+y;
-// console.log(`sum is:${sum}`); //evabe backtik er vitor curley bracket er modhhe value print kora jay
-
-
-let name="mithun banerjee";
-let message=`i am ${name} now i am learning es6`;
-console.log(message);
+//function use korte hole 'const' keyword use korte hobe .tarpor parameter '='pore dite hobe then '=>'diy '{}'use korte hobe
+const add=(x,y)=>{
+   let sum =x+y;
+   console.log(sum);
+}
+add(20,10);
