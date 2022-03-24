1ef7e05819acff1664956ffbe0b59d025115c900
Mithun Banerjee <34417291+mithunbanerjee632@users.noreply.github.com>
2022-03-25 04:00:13 +0600

    exaple of arrow function with parameter  with bracket and return and  without return and curley bracket

--- main.js
+++ main.js
@@ -14,12 +14,37 @@
 // display2();
 
 //const display2 =()=>{console.log("this is Arrow function");}
-const display2 =()=>console.log("this is Arrow function"); //arrow function e curley bracket na dileo hobe
-display2();
-const display3 =()=>{return "this is Arrow function example1";}
-console.log(display3());
+//exaple1
+// const display2 =()=>console.log("this is Arrow function"); //arrow function e curley bracket na dileo hobe
+// display2();
+//exaple2
+// const display3 =()=>{return "this is Arrow function example1";}
+// console.log(display3());
+//exaple3
+// const display4 =()=>"this is Arrow function example2"; //arrow te return and curley bracket na dileo hobe
+// console.log(display4());
 
-const display4 =()=>"this is Arrow function example2"; //arrow te return and curley bracket na dileo hobe
-console.log(display4());
 
+//traditional function with parameter
 
+function add1(num1,num2){
+  return num1+num2;
+}
+
+console.log(add1(10,20));
+
+//Arrow function with parameter
+
+//example with return and curley bracket
+
+const add2 = (num1,num2) => {
+   return num1+num2;
+ }
+ 
+ console.log(add2(30,20));
+
+ //example without return and curley bracket
+
+ const add3 = (num1,num2) => num1+num2;
+ 
+ console.log(add3(50,20));
