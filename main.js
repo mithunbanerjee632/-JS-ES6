10e58ec48e5285bb6b0ae8789463cc018d450869
Mithun Banerjee <34417291+mithunbanerjee632@users.noreply.github.com>
2022-03-25 03:29:42 +0600

    filtering array by filter method

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 //map and filter array function
 
 //map
-var numbers =[5,6,7,8,9];
-var sqrtNumbers=numbers.map(function(x){
-   return(x*x);
+// var numbers =[5,6,7,8,9];
+// var sqrtNumbers=numbers.map(function(x){
+//    return(x*x);
+// })
+
+// console.log(sqrtNumbers);
+
+//Filter
+var numbers =[11,50,5,8,9,22,12,26];
+var filterNumbers=numbers.filter(function(x){
+   return x>10;
 })
 
-console.log(sqrtNumbers);
+console.log(filterNumbers);
