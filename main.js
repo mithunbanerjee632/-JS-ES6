1018f03084c918b8b27a82e48d12c95b848abbef
Mithun Banerjee <34417291+mithunbanerjee632@users.noreply.github.com>
2022-03-25 03:49:36 +0600

    exaple of arrow function

--- main.js
+++ main.js
@@ -1,14 +1,25 @@
 //Arrow Function
 
 //traditional function 
-function display1(){
-   console.log("this is tradition function");
-}
-display1();
+// function display1(){
+//    console.log("this is tradition function");
+// }
+// display1();
 
 //Arrow function 
-const display2 =()=>{
-   console.log("this is Arrow function");
+// const display2 =()=>{
+//    console.log("this is Arrow function");
 
-}
+// }
+// display2();
+
+//const display2 =()=>{console.log("this is Arrow function");}
+const display2 =()=>console.log("this is Arrow function"); //arrow function e curley bracket na dileo hobe
 display2();
+const display3 =()=>{return "this is Arrow function example1";}
+console.log(display3());
+
+const display4 =()=>"this is Arrow function example2"; //arrow te return and curley bracket na dileo hobe
+console.log(display4());
+
+
