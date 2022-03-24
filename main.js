006d5cf070cd6beafacafb7c62c53329ec17ddae
mithun-banerjee <mithunbanerjee632@gmail.com>
2022-03-24 22:05:05 +0600

    without sprade operator.Using push()

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-//Sprade Operator: three dots(...)
+//without Sprade Operator: three dots(...)
 let poorCountry =['Bangladesh','Srilanka','Bhutan'];
-let richCountry =[...poorCountry,'America','Canada','Italy'];
-console.log('richCountry');
+let richCountry =['America','Canada','Italy'];
+//console.log('richCountry')
+richCountry.push(poorCountry);
