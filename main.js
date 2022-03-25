ef40228d3e4476fdc7ce0ec942a5d6c7b4173646
Mithun Banerjee <34417291+mithunbanerjee632@users.noreply.github.com>
2022-03-25 13:16:04 +0600

    array destructuring

--- main.js
+++ main.js
@@ -1,47 +1,8 @@
-//Arrow Function
+//Array Destructuring
 
-//traditional function 
- var student =[
-   {
-     id:101,
-     name:'mithun',
-     cgpa:2.30
-   },
-   {
-    id:102,
-    name:'puja',
-    cgpa:3.30
-  },
-  {
-    id:103,
-    name:'mridul',
-    cgpa:4.30
-  },
-  {
-    id:104,
-    name:'partho',
-    cgpa:4.65
-  }
- ]
+let numbers =[10,20,30,40,50]
+//let [num1,num2,num3,num4,num5]=numbers //array er value gulo k variable er vitor assign kora hoice
+let [num1,num2,...z]=numbers //array er value gulo k variable er vitor assign kora hoice
 
- function studentNames1(){
-   return student.filter(function(x){
-      return x.cgpa>3;  //jader cgpa 3 er upore tader details return korbe
-   }).map(function(y){
-      return y.name;    //jader cgpa 3 er upore tader name return korbe
-   });
-
-   
- }
-
- //console.log(studentNames1());
-
-//Arrow function with map and filter
-
-// const studentNames2=()=>{
-//   return student.filter((x)=>x.cgpa>3).map((y)=>y.name);   //with bracket and return
-// }
-
-const studentNames2=()=>student.filter((x)=>x.cgpa>3).map((y)=>y.name); ///without bracket and return
-
-console.log(studentNames2());
+//console.log(num1);
+console.log(z);
