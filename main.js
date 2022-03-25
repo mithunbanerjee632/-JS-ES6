7c7318f6ba53d033e72ee3061d64ce9d31a2ca0c
Mithun Banerjee <34417291+mithunbanerjee632@users.noreply.github.com>
2022-03-26 03:10:57 +0600

    constructor() in Class

--- main.js
+++ main.js
@@ -1,31 +1,9 @@
-//class:class onek gulo property k hold kore rakhte pare..class er object k call kore amra class er vitor property k bar bar reuse korte parbo
-
+//constructor: constructor hocce class er nijossho function or method.class er object jokhon create kora hoi tokhon constructor automatic call hoy.onnano function er moto constructor sudhu return korte pare na
 class myClass{
-  myFun1(name){
-    console.log(name);
-  }
-  myFun2(name){
-    console.log(name);
-  }
-  myFun3(name){
-    console.log(name);
-  }
-  myFun4(name){
-    console.log(name);
-  }
-  myFun5(name){
-    console.log(name);
-  }
-  myFun6(name){
-    console.log(name);
+  constructor(){
+    console.log("i am a Constructor");
   }
 }
-let obj = new myClass();
+new myClass();
 
-obj.myFun1("Bangladesh");
-obj.myFun2("India");
-obj.myFun3("Nepal");
-obj.myFun4("Bhutan");
-obj.myFun5("Srilanka");
-obj.myFun6("Maldiv");
 
