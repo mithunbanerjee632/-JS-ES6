5235b776592f77e62f84eb694501417d2c4ab90c
Mithun Banerjee <34417291+mithunbanerjee632@users.noreply.github.com>
2022-03-26 02:55:43 +0600

    class in ES6

--- main.js
+++ main.js
@@ -1,24 +1,31 @@
-//Set:collection of data.as like as map...but map er vitor duplicate value rakha jay ,set er vitor duplicate value rakha jay na
+//class:class onek gulo property k hold kore rakhte pare..class er object k call kore amra class er vitor property k bar bar reuse korte parbo
 
-var mySet = new Set();
-
-mySet.add("Bangladesh");
-mySet.add("India");
-mySet.add("Nepal");
-mySet.add("Bhutan");
-mySet.add("Srilanka");
-mySet.add("Maldiv");
-mySet.add("India"); //ei duplicate value avoid korbe
-
-//console.log(mySet);
-//mySet.clear(); //sob kicu remove kore dibe
-//mySet.delete("Maldiv"); //specific value k delete kore dibe
-//console.log(mySet.size); // set er size ber korbe
-//console.log(mySet.values());
-
-if(mySet.has("India")){
-  console.log("Yes");
-}else{
-  console.log("No");
+class myClass{
+  myFun1(){
+    console.log("i am function1 from inside class");
+  }
+  myFun2(){
+    console.log("i am function2 from inside class");
+  }
+  myFun3(){
+    console.log("i am function3 from inside class");
+  }
+  myFun4(){
+    console.log("i am function4 from inside class");
+  }
+  myFun5(){
+    console.log("i am function5 from inside class");
+  }
+  myFun6(){
+    console.log("i am function6 from inside class");
+  }
 }
- 
+let obj = new myClass();
+
+obj.myFun1();
+obj.myFun2();
+obj.myFun3();
+obj.myFun4();
+obj.myFun5();
+obj.myFun6();
+
