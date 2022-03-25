c76712f70e2655274e0e52bff077e25859dd75b5
Mithun Banerjee <34417291+mithunbanerjee632@users.noreply.github.com>
2022-03-25 14:30:01 +0600

    string methods:startsWith(),endsWith(),includes()

--- main.js
+++ main.js
@@ -1,44 +1,12 @@
-//Array method :find() and findIndex()
-//find(callback,value)->return the value of the first element that match the certain condition
-// let numbers1 =[5,55,14,78,22]
-// let evenNumber=numbers1.find(x => x%2 === 0) //first jar sathe condition match hobe seta return korbe
-// console.log(evenNumber);
+//startsWith(searchString,position):check a strign starts with another string
 
-// //findIndex():return the value of the first Index number which match the certain condition
+const message ="Today is friday"
+console.log(message.startsWith('Today')); //initially index 0 thake
 
-// let numbers2 =[5,55,14,78,22]
-// let evenNumberIndex=numbers2.findIndex(x => x%2 === 0) //first jar sathe condition match hobe tar index  return korbe
-// console.log(evenNumberIndex);
+//endsWith(searchString,length):check a strign ends with another string
+console.log(message.endsWith('friday'));
 
-//find() method with object
+//include(searchString,position):check if a string contains another string
+console.log(message.includes('friday'));
 
-
-let students =[
-  {
-    id:101,
-    gpa:2.65
-  },
-  {
-    id:102,
-    gpa:2.65
-  },
-
-  {
-    id:103,
-    gpa:3.65
-  },
-
-  {
-    id:104,
-    gpa:4.65
-  },
-
-  {
-    id:105,
-    gpa:5.00
-  }
-  
-  
-]
-
-console.log(students.find(x=>x.gpa>4));
+//all conditions are case sensitive
