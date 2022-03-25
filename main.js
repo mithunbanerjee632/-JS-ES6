f454f6c4085db0e2f9123e2d9a40678692ecfd51
Mithun Banerjee <34417291+mithunbanerjee632@users.noreply.github.com>
2022-03-26 00:50:58 +0600

    Array in ES6

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
-//startsWith(searchString,position):check a strign starts with another string
+//array
+var myArray =["A","B","C","D"]
+var myArrayPro=new Array("A","B","C","D")
 
-const message ="Today is friday"
-console.log(message.startsWith('Today')); //initially index 0 thake
-
-//endsWith(searchString,length):check a strign ends with another string
-console.log(message.endsWith('friday'));
-
-//include(searchString,position):check if a string contains another string
-console.log(message.includes('friday'));
-
-//all conditions are case sensitive
+console.log(myArray[0]);
+for(let item of myArray){
+  console.log(item);
+}
