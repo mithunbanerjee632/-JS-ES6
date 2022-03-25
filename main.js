00812332852ec9bf126531e91e5bfcbca00de3e5
Mithun Banerjee <34417291+mithunbanerjee632@users.noreply.github.com>
2022-03-26 01:45:53 +0600

    delete() in map

--- main.js
+++ main.js
@@ -7,13 +7,13 @@ myMap.set("key4","Maldives");
 myMap.set("key5","Bhutan");
 myMap.set("key6","Afghanistan");
 myMap.set("key7","Pakistan");
-console.log(myMap.values()); //value er jonno values() method call korte hobe..egulor value array return kore
-console.log(myMap.keys()); //Key er jonno Keys() method call korte hobe.egulor Key array return kore
+//console.log(myMap.values()); //value er jonno values() method call korte hobe..egulor value array return kore
+//console.log(myMap.keys()); //Key er jonno Keys() method call korte hobe.egulor Key array return kore
+
+myMap.delete("key1")
+myMap.delete("key2") //key dhore deleter kore dibe 
 
 for(let myvalue of myMap.values()){   // jehetur array return kore tai for of loof chalate hobe
     console.log(myvalue);
 }
 
-for(let mykey of myMap.keys()){
-  console.log(mykey);
-}
