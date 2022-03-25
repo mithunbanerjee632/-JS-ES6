cd4124a90295ab30178955f98eb08f0a615075eb
Mithun Banerjee <34417291+mithunbanerjee632@users.noreply.github.com>
2022-03-26 03:19:29 +0600

    constructor parameter in ES6

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 //constructor: constructor hocce class er nijossho function or method.class er object jokhon create kora hoi tokhon constructor automatic call hoy.onnano function er moto constructor sudhu return korte pare na
 class myClass{
-  constructor(){
-    console.log("i am a Constructor");
+  constructor(a,b){
+    //console.log(a+b);
+    this.firstNum = a //class variable
+    this.secondNum=b
   }
+
+  add(){
+    console.log(this.firstNum+this.secondNum);
+  }
+
 }
-new myClass();
+let obj = new myClass(10,20);
+obj.add();
+
 
 
