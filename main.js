0da852a3b6a4aaf6c51d51a5f0cf01d0480943b1
Mithun Banerjee <34417291+mithunbanerjee632@users.noreply.github.com>
2022-03-26 01:36:56 +0600

    map in For of loop

--- main.js
+++ main.js
@@ -7,5 +7,13 @@ myMap.set("key4","Maldives");
 myMap.set("key5","Bhutan");
 myMap.set("key6","Afghanistan");
 myMap.set("key7","Pakistan");
-console.log(myMap.values()); //value er jonno values() method call korte hobe
-console.log(myMap.keys()); //Key er jonno Keys() method call korte hobe
+console.log(myMap.values()); //value er jonno values() method call korte hobe..egulor value array return kore
+console.log(myMap.keys()); //Key er jonno Keys() method call korte hobe.egulor Key array return kore
+
+for(let myvalue of myMap.values()){   // jehetur array return kore tai for of loof chalate hobe
+    console.log(myvalue);
+}
+
+for(let mykey of myMap.keys()){
+  console.log(mykey);
+}
