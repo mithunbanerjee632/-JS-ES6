3be4ddb0ee3ae0016439cbebd22fdf3d0affa856
Mithun Banerjee <34417291+mithunbanerjee632@users.noreply.github.com>
2022-03-26 00:59:53 +0600

    multidimensional array

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 //array
-var myArray =["A","B","C","D"]
-var myArrayPro=new Array("A","B","C","D")
+// var myArray =["A","B","C","D"]
+// var myArrayPro=new Array("A","B","C","D")
 
-console.log(myArray[0]);
-for(let item of myArray){
-  console.log(item);
-}
+// console.log(myArray[0]);
+// for(let item of myArray){
+//   console.log(item);
+// }
+
+//multidimensional array
+ var bangladesh = ["Dhaka","Rangpur","Sylhet","Bairshal"]
+ var india = ["Kolkata","Mumbai","Chennai","Delhi"]
+ var china = ["Wuhan","Sanghai","Beijing"]
+
+ var asia =[bangladesh,india,china]
+
+ console.log(asia[1][2]);
+ console.log(asia[0][0]);
