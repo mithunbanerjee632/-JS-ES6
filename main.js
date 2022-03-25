5e0cc1e5ea165cde427e3ff8d45a757b00ac8e82
Mithun Banerjee <34417291+mithunbanerjee632@users.noreply.github.com>
2022-03-26 03:00:27 +0600

    function parameter passing in ES6

--- main.js
+++ main.js
@@ -1,31 +1,31 @@
 //class:class onek gulo property k hold kore rakhte pare..class er object k call kore amra class er vitor property k bar bar reuse korte parbo
 
 class myClass{
-  myFun1(){
-    console.log("i am function1 from inside class");
+  myFun1(name){
+    console.log(name);
   }
-  myFun2(){
-    console.log("i am function2 from inside class");
+  myFun2(name){
+    console.log(name);
   }
-  myFun3(){
-    console.log("i am function3 from inside class");
+  myFun3(name){
+    console.log(name);
   }
-  myFun4(){
-    console.log("i am function4 from inside class");
+  myFun4(name){
+    console.log(name);
   }
-  myFun5(){
-    console.log("i am function5 from inside class");
+  myFun5(name){
+    console.log(name);
   }
-  myFun6(){
-    console.log("i am function6 from inside class");
+  myFun6(name){
+    console.log(name);
   }
 }
 let obj = new myClass();
 
-obj.myFun1();
-obj.myFun2();
-obj.myFun3();
-obj.myFun4();
-obj.myFun5();
-obj.myFun6();
+obj.myFun1("Bangladesh");
+obj.myFun2("India");
+obj.myFun3("Nepal");
+obj.myFun4("Bhutan");
+obj.myFun5("Srilanka");
+obj.myFun6("Maldiv");
 
