42fcb224f2cf7e082d8f0eda029b1efd15f3aa92
Mithun Banerjee <34417291+mithunbanerjee632@users.noreply.github.com>
2022-03-25 13:21:13 +0600

    swappin value by destructuring

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 //Array Destructuring
 
-let numbers =[10,20,30,40,50]
+//let numbers =[10,20,30,40,50]
 //let [num1,num2,num3,num4,num5]=numbers //array er value gulo k variable er vitor assign kora hoice
-let [num1,num2,...z]=numbers //array er value gulo k variable er vitor assign kora hoice
+//let [num1,num2,...z]=numbers //array er value gulo k variable er vitor assign kora hoice
 
 //console.log(num1);
-console.log(z);
+//console.log(z);
+
+//swap variables
+
+let a=20,b=30;
+
+[a,b]=[b,a]
+
+console.log(a);
+console.log(b);
