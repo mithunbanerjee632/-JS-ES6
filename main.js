b6662a3e66608cda9d3558f1987c1f3adebfb3bd
Mithun Banerjee <34417291+mithunbanerjee632@users.noreply.github.com>
2022-03-26 11:37:36 +0600

    class inheritence concept and exaple in ES6

--- main.js
+++ main.js
@@ -1,16 +1,25 @@
-//static property:static er somporko holo class er sathe object er sathe noy
+//Class Inhertitence:uttoradhikar sutre parent class er sob property child class peye jay
 
-class myClass{
-  nonStaticProperty(){
-    console.log("i am non -static property");
+class parent{
+  hello1(){
+    console.log("Hello1 from parent class");
   }
 
-  static StaticProperty(){
-    console.log("i am a static property");
+  hello2(){
+    console.log("Hello2 from parent class");
   }
 }
 
-var obj = new myClass();
-obj.nonStaticProperty(); // static property na hole object create kore call korte hoy
+class child extends parent{
 
-myClass.StaticProperty(); //static property hole class diye sorasori call korte hoy
+  hello1(){
+    console.log("Hello1 from function..hello hello...i am from child"); //method overriding cz parent er theke child er provab besi
+  }
+
+}
+
+var obj = new child();
+//var obj = new parent();
+
+obj.hello1();
+//obj.hello2();
